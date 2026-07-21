4e0833e872cfa59c3aeee5866a6ac3907326d919
TienHM12 <minhtien250704@gmail.com>
2026-07-21 23:58:09 +0700

    update

--- apps/topic-follower-service/src/app/app.controller.ts
+++ apps/topic-follower-service/src/app/app.controller.ts
@@ -6,31 +6,18 @@ import {
   Param,
   Post,
   Req,
-<<<<<<< HEAD
   UseGuards, // Thêm UseGuards
   UseInterceptors,
   UnauthorizedException, // Thêm để quăng lỗi 401 nếu không có user
-=======
-  UseGuards,
-  UseInterceptors,
-  UnauthorizedException,
->>>>>>> d9b0b30 (update)
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SubscribeTopicDto } from '@bus/models';
 import { InjectUserInterceptor } from '@bus/common';
-<<<<<<< HEAD
 // Sửa lại đường dẫn import Guard cho đúng với cấu trúc dự án của Tiến
 import { FirebaseAuthGuard } from '@bus/common';
 
 @Controller('topic-followers')
 @UseGuards(FirebaseAuthGuard) // 🚨 QUAN TRỌNG: Phải gắn Guard này vào để nó giải mã Token
-=======
-import { FirebaseAuthGuard } from '@bus/common';
-
-@Controller('topic-followers')
-@UseGuards(FirebaseAuthGuard)
->>>>>>> d9b0b30 (update)
 @UseInterceptors(InjectUserInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -38,10 +25,7 @@ export class AppController {
   // 1. GET: Lấy danh sách ID các topic đã theo dõi
   @Get('my-subscriptions')
   async getMySubscriptions(@Req() req: any) {
-<<<<<<< HEAD
     // Thêm dấu ? để chống crash lỗi 500. Fallback giữa user_id và uid
-=======
->>>>>>> d9b0b30 (update)
     const userId = req.user?.user_id || req.user?.uid;
 
     if (!userId)
--- libs/common/src/firebase/firebase-auth.guard.ts
+++ libs/common/src/firebase/firebase-auth.guard.ts
@@ -4,19 +4,11 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-<<<<<<< HEAD
-import { getAuth } from 'firebase-admin/auth'; // 🚨 Thay đổi cách import ở đây
-=======
 import { getAuth } from 'firebase-admin/auth';
->>>>>>> d9b0b30 (update)
 
 @Injectable()
 export class FirebaseAuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-<<<<<<< HEAD
-    // Sửa lại thành boolean cho đúng cú pháp TS
-=======
->>>>>>> d9b0b30 (update)
     const request = context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization;
 
@@ -27,15 +19,8 @@ export class FirebaseAuthGuard implements CanActivate {
     const token = authHeader.split(' ')[1];
 
     try {
-<<<<<<< HEAD
-      // 🚨 Dùng getAuth() trực tiếp thay vì admin.auth()
       const decodedToken = await getAuth().verifyIdToken(token);
 
-      // Gán thông tin user vào request để Controller và Interceptor dùng được
-=======
-      const decodedToken = await getAuth().verifyIdToken(token);
-
->>>>>>> d9b0b30 (update)
       request.user = decodedToken;
 
       return true;
