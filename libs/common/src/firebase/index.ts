99da94228d59193d732d2aa3fa2c9f731c07cb59
TienHM12 <minhtien250704@gmail.com>
2026-07-22 08:34:32 +0700

    update

--- libs/common/src/firebase/index.ts
+++ libs/common/src/firebase/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export * from './firebase.module';
-export * from './firebase.service';
-export * from './firebase-auth.guard'; // Thêm dòng này
-=======
 export * from './firebase.module.js';
 export * from './firebase.service.js';
 export * from './firebase-auth.guard.js';
->>>>>>> d9b0b30 (update)
